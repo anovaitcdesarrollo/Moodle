d48ed7d4bebd92a6c7482a6b0f86d0a398a48608
Anova IT Consulting <anovaitcdesarrollo@users.noreply.github.com>
2021-01-28 20:19:08 +0100

    Corregido nombre de archivo helper_functions

--- azure_scripts/setup_webserver_2101.sh
+++ azure_scripts/setup_webserver_2101.sh
@@ -25,7 +25,7 @@ set -ex
 
 moodle_on_azure_configs_json_path=${1}
 
-. ./helper_functions.sh
+. ./helper_functions_2101.sh
 
 get_setup_params_from_configs_json $moodle_on_azure_configs_json_path || exit 99
 
